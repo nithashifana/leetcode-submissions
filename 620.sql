608fc99e7da76ef2aae944d17f8fdf7d5a5d2c04
Nitha Shifana <nithashifana@gmail.com>
2025-01-03 09:20:04 +0530

    created

---
+++ 620.sql
@@ -0,0 +1,8 @@
+# Not boring movies
+# Easy
+
+# Write your MySQL query statement below
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id%2 = 1 AND NOT description = "boring"
+ORDER BY rating DESC;
