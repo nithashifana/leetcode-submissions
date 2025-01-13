88d4605fc96eb4a67357af996f523d17a2d0ce9b
Nitha Shifana <nithashifana@gmail.com>
2025-01-13 09:23:01 +0530

    created

---
+++ 2116.cpp
@@ -0,0 +1,34 @@
+// Check if a parentheses string can be valid
+// Medium
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canBeValid(string str, string lockStatus) {
+        if (str.size() % 2 != 0) 
+            return false;
+
+        int openCount = 0;
+        for (int i = 0; i < str.size(); ++i) {
+            if (lockStatus[i] == '0' || str[i] == '(') 
+                openCount++;
+            else 
+                openCount--;
+            if (openCount < 0) 
+                return false;
+        }
+
+        openCount = 0;
+        for (int i = str.size() - 1; i >= 0; --i) {
+            if (lockStatus[i] == '0' || str[i] == ')') 
+                openCount++;
+            else 
+                openCount--;
+            if (openCount < 0) 
+                return false;
+        }
+        return true;
+    }
+};
