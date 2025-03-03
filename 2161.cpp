4f5a86b05149ff35696407553f21a60407afe87b
Nitha Shifana <nithashifana@gmail.com>
2025-03-03 10:07:45 +0530

    created

---
+++ 2161.cpp
@@ -0,0 +1,28 @@
+// Partition array according to given pivot
+// Medium
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> arr;
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] < pivot) {
+                arr.push_back(nums[i]); 
+            }
+        }
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] == pivot) {
+                arr.push_back(nums[i]); 
+            }
+        }
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] > pivot) {
+                arr.push_back(nums[i]); 
+            }
+        }
+        return arr;
+    }
+};
