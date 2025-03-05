ed54e22c76f7da766414b4097f3dc80da3648dfa
Nitha Shifana <nithashifana@gmail.com>
2025-03-05 11:49:00 +0530

    created

---
+++ 2481.cpp
@@ -0,0 +1,14 @@
+// Minimum cuts to divide a circle
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if(n == 1) return 0;
+        if(n % 2 == 0) return n/2;
+        return n; 
+    }
+};
