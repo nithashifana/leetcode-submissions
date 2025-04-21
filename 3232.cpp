feb011f4c7913933f75dabdafeb3a63d6c7141ad
Nitha Shifana <nithashifana@gmail.com>
2025-04-22 00:47:19 +0530

    created

---
+++ 3232.cpp
@@ -0,0 +1,17 @@
+// Find if digit game can be won
+// Easy
+
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int o = 0, t = 0;
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] / 10 == 0) {
+                o += nums[i];
+            }
+            else t += nums[i];
+        }
+
+        return (o != t);
+    }
+};
