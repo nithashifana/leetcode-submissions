87a0a828c01d0a82a91f9d82daacbbf0d0049bdc
Nitha Shifana <nithashifana@gmail.com>
2025-05-05 12:02:02 +0530

    created

---
+++ 412.cpp
@@ -0,0 +1,23 @@
+// Fizz Buzz
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> arr(n);
+        for(int i = 1; i <= n; i ++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                arr[i-1] = "FizzBuzz";
+            } else if(i % 3 == 0) {
+                arr[i-1] = "Fizz";
+            } else if(i % 5 == 0) {
+                arr[i-1] = "Buzz";
+            }
+            else arr[i-1] = to_string(i);
+        }
+        return arr;
+    }
+};
