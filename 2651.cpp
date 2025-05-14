1e5f72788448a74f5e0e0b7685adea5a179b898c
Nitha Shifana <nithashifana@gmail.com>
2025-05-14 22:16:46 +0530

    created

---
+++ 2651.cpp
@@ -0,0 +1,12 @@
+// Calculate delayed arrival time
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+};
