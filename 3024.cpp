c087718b446f5dab059cdc77994b4f9b084e7b5a
Nitha Shifana <nithashifana@gmail.com>
2025-05-19 22:14:48 +0530

    created

---
+++ 3024.cpp
@@ -0,0 +1,26 @@
+// Type of triangle
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if (nums[0] == nums[1] && nums[1] == nums[2] &&
+            nums[0] + nums[1] > nums[2] && nums[0] < nums[1] + nums[2] &&
+            nums[0] + nums[2] > nums[1])
+            return "equilateral";
+        else if ((nums[0] == nums[1] && nums[0] != nums[2] ||
+                  nums[2] == nums[1] && nums[0] != nums[2] ||
+                  nums[0] == nums[2] && nums[1] != nums[2]) &&
+                 (nums[0] + nums[1] > nums[2] && nums[0] < nums[1] + nums[2] &&
+                  nums[0] + nums[2] > nums[1]))
+            return "isosceles";
+        else if (nums[0] + nums[1] > nums[2] && nums[0] < nums[1] + nums[2] &&
+                 nums[0] + nums[2] > nums[1])
+            return "scalene";
+        else
+            return "none";
+    }
+};
