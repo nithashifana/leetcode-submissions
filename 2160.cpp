66203df74ac22a4fffb659f1aa20638b3279dcee
Nitha Shifana <nithashifana@gmail.com>
2025-05-22 22:35:09 +0530

    created

---
+++ 2160.cpp
@@ -0,0 +1,21 @@
+// Minimum sum of four digit number after splitting
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> arr;
+        while(num) {
+            arr.push_back(num%10);
+            num /= 10;
+        }
+        sort(arr.begin(), arr.end());
+        int n1 = 0, n2 = 0; 
+        for(int i = 0; i < arr.size(); i += 2) n1 = n1*10 + arr[i];
+        for(int i = 1; i < arr.size(); i += 2) n2 = n2*10 + arr[i];
+        return n1 + n2;
+    }
+};
