ee89ae82697eaa907dfa6e4626c9750cef70affd
Nitha Shifana <nithashifana@gmail.com>
2025-05-25 09:40:05 +0530

    created

---
+++ 2089.cpp
@@ -0,0 +1,17 @@
+// Find target indices aftersorting array
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> arr;
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] == target) arr.push_back(i);
+        }
+        return arr;
+    }
+};
