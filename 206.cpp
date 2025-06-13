c43aabe1b6b6a07610ac076abac3425082b46132
Nitha Shifana <nithashifana@gmail.com>
2025-06-13 19:48:26 +0530

    created

---
+++ 206.cpp
@@ -0,0 +1,28 @@
+// Reverse Linked List
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* prev = nullptr;
+        while(current) {
+            ListNode* temp = current->next;
+            current->next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;;
+    }
+};
