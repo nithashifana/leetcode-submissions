4f074af0bb8eb83643c76cdd53cfd3d3538113ca
Nitha Shifana <nithashifana@gmail.com>
2025-06-20 09:41:19 +0530

    created

---
+++ 225.cpp
@@ -0,0 +1,36 @@
+// Implement Stack using Queues
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 1; i < q.size(); i ++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
