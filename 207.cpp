cf95f8ef1df79ea2c3c5debbc98558cd2366c061
Nitha Shifana <nithashifana@gmail.com>
2025-06-27 12:14:29 +0530

    created

---
+++ 207.cpp
@@ -0,0 +1,38 @@
+// Course Schedule
+// Medium
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        
+        for (auto& pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto& it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.push(it);
+            }
+        }
+
+        return topo.size() == numCourses;
+    }
+};
