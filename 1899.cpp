b7d8e55a950e3fb5338345c1a068e962aec432f5
Nitha Shifana <nithashifana@gmail.com>
2025-06-30 18:44:38 +0530

    created

---
+++ 1899.cpp
@@ -0,0 +1,20 @@
+// Merge Triplets to Form Target Triplet
+// Medium
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        vector<int> ans(3, -1);
+        for(auto &it : triplets) {
+            if(it[0] <= target[0] && it[1] <= target[1] && it[2] <= target[2]) {
+                ans[0] = max(ans[0], it[0]);
+                ans[1] = max(ans[1], it[1]);
+                ans[2] = max(ans[2], it[2]);
+            }
+        }
+        return ans == target;;
+    }
+};
