130a03a8ca8b4cf4f9cfcd7a16dd43d1d15f9e3c
Nitha Shifana <nithashifana@gmail.com>
2026-03-04 22:16:51 +0530

    created

---
+++ 1582.cpp
@@ -0,0 +1,32 @@
+// Special Positions in a Binary Matrix
+// Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int k = 0;
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                if(mat[i][j]) {
+                    row[i] ++;
+                    col[j] ++;
+                }
+            }
+        }
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    k ++;
+                }
+            }
+        }
+        return k;
+    }
+};
